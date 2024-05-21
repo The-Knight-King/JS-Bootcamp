cf0badb487240a7eadccb1447077e3170acbe1ab
The-Knight-King <akshitmittal98@gmail.com>
2024-05-21 19:39:57 +0530

    Understanding the variables and difference between var, let and const

--- first.js
+++ first.js
@@ -1,3 +1,41 @@
 console.log("Hello World!")
 
 console.log("Wake up to Reality!")
+
+// Learning about variables in JS
+
+fullName = "Tony Stark"
+age = 45
+
+console.log("Hi! My name is " + fullName + " and I am " + age + " years old.")
+
+// Understanding var, let and const
+
+var num = 34
+
+var num = 45
+
+var num = 89
+
+console.log(num)
+
+// Seeing how to declare using `let`
+
+let x = 45
+
+x = 67
+
+x = 78
+
+console.log(x)
+
+// Seeing how to declare using `const`
+
+const y = 45
+
+y = 567
+
+y = 678
+
+console.log()
+
