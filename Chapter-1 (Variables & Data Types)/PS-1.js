222ad28221218f8b11dcb1bc31332eed146dc3a2
The-Knight-King <akshitmittal98@gmail.com>
2024-05-21 22:03:30 +0530

    completed practice set-1 and organised files inside parent folder

---
+++ Chapter-1 (Variables & Data Types)/PS-1.js
@@ -0,0 +1,25 @@
+// This is our PQ-1, see the index.html file for info
+
+const product = {
+    prodName:"Parker Jotter Standard CT",
+    penType:"Ball Pen",
+    color:"Black",
+    rating:4,
+    price:270,
+    isDealOfTheDay:true
+}
+
+console.log(product)
+
+// This is our PQ-2, see the index.html file for more info
+
+const profile = {
+    userName:"@shradhakhapra",
+    fullName:"Shradha Khapra",
+    posts:195,
+    followers:569000,
+    following:4,
+    isFollowed: false,
+}
+
+console.log(profile)
