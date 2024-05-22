51bc0473a37b1918e8eb32448c8ff804b63b5821
The-Knight-King <akshitmittal98@gmail.com>
2024-05-22 21:41:20 +0530

    Completed pratice question-1

---
+++ Chapter-3 (Loops and Strings)/practice set 3.js
@@ -0,0 +1,10 @@
+// Q1. Print all the even no. 0 to 100. 
+
+console.log("Printing all the even nos. till 100")
+
+for (let i=0;i<=100;i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+};
+
