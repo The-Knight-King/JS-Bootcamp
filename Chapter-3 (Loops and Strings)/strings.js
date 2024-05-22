14959bd40316b2f358f5560d3002bdcb62687820
The-Knight-King <akshitmittal98@gmail.com>
2024-05-22 22:08:51 +0530

    learning abt strings

---
+++ Chapter-3 (Loops and Strings)/strings.js
@@ -0,0 +1,14 @@
+// Strings in JS
+
+abc = "Apna college"
+console.log(abc)
+
+// Strings in JS have some inbuilt properties and functions, let's see them.
+
+str = "Javascript is fun"
+console.log(str)
+console.log("Length of str is:",str.length)    //length function
+
+// string indices
+
+console.log(str[0],str[5],str[8])
