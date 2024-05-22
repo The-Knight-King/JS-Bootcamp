cab76ed4c554605f9f5cbdd95f3d8bd698c7bc9b
The-Knight-King <akshitmittal98@gmail.com>
2024-05-22 23:34:43 +0530

    learning abt string properties and template literals

--- Chapter-3 (Loops and Strings)/strings.js
+++ Chapter-3 (Loops and Strings)/strings.js
@@ -12,3 +12,20 @@ console.log("Length of str is:",str.length)    //length function
 // string indices
 
 console.log(str[0],str[5],str[8])
+
+// Template Literals in JS
+// we can print strings using `` in the form of template literals.
+
+let obj = {
+    name: "pen",
+    price: 20,
+};
+
+console.log(`The price of ${obj.name} is $${obj.price}.`)   //printing data using this format is known as `String interpolation` in JS and is used commonly.
+
+// escape characters inside strings in JS like \n, \t
+
+console.log("Hi there!\nI hope you are doing well.");
+let string = "Happy\tWeekend";  //str length is 13 here instead of 14 bcoz \t is treated only as a single character.
+console.log("String:",string);
+console.log(`Lenth of the above string is: ${string.length}`);
