8ba4d2e7d7c5acd096234afb498cf911c5b97ba6
The-Knight-King <akshitmittal98@gmail.com>
2024-05-22 17:34:56 +0530

    learning while and do-while loop

--- Chapter-3 (Loops and Strings)/loops.js
+++ Chapter-3 (Loops and Strings)/loops.js
@@ -10,12 +10,30 @@ console.log("Loop has ended")
 
 // Calculate sum of first n numbers
 
-let num = prompt("Enter a number till which you want the sum of all numbers:");
+// let num = prompt("Enter a number till which you want the sum of all numbers:");
 
-let sum = 0;
+// let sum = 0;
 
-for (a=1; a<=num; a++) {
-    sum = sum + a;
-}
+// for (a=1; a<=num; a++) {
+//     sum = sum + a;
+// }
 
-console.log("Sum of all the natural numbers till",num,"is:",sum)
+// console.log("Sum of all the natural numbers till",num,"is:",sum)
+
+// While loop
+// console.log("printing using while loop below")
+// let a = 1;
+// while (a<=10) {
+//     console.log("a:",a);
+//     a++;
+// }
+
+// do-while loop -> it works exactly like the while loop but there is just a smnall difference between both which is that the in while loop the condition is checked in the very start and it will only execute if the condition is true, but in case of do-while the condition is checked later and it guranteed that the loop will execute at least 1 time.
+
+console.log("printing using do-while loop below");
+
+let a = 20;
+do {
+    console.log("a:",a);
+    a++;
+} while (a<=10);
