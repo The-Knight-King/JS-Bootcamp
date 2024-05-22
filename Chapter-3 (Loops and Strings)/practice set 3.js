f01b144a712fcfab24ce8452c857ddec4d5396fb
The-Knight-King <akshitmittal98@gmail.com>
2024-05-22 21:57:21 +0530

    Completed pratice question-2

--- Chapter-3 (Loops and Strings)/practice set 3.js
+++ Chapter-3 (Loops and Strings)/practice set 3.js
@@ -1,10 +1,21 @@
 // Q1. Print all the even no. 0 to 100. 
 
-console.log("Printing all the even nos. till 100")
+// console.log("Printing all the even nos. till 100")
 
-for (let i=0;i<=100;i++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
-};
+// for (let i=0;i<=100;i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// };
 
+// Q2. Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters the correct value.
+
+let gameNum = 10;
+
+let userNum = prompt("Guess a number between 0 and 10:");
+
+while(userNum != gameNum) {
+    userNum = prompt("You guessed the wrong number! Guess again:");
+}
+
+alert("Congrats! you guessed it right!!")
