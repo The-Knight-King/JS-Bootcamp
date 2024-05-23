d90cd8aeccd1431c255baed115d57231c4ded876
The-Knight-King <akshitmittal98@gmail.com>
2024-05-23 11:56:00 +0530

    Completed practice set 3

--- Chapter-3 (Loops and Strings)/practice set 3.js
+++ Chapter-3 (Loops and Strings)/practice set 3.js
@@ -10,12 +10,23 @@
 
 // Q2. Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters the correct value.
 
-let gameNum = 10;
+// let gameNum = 10;
 
-let userNum = prompt("Guess a number between 0 and 10:");
+// let userNum = prompt("Guess a number between 0 and 10:");
 
-while(userNum != gameNum) {
-    userNum = prompt("You guessed the wrong number! Guess again:");
-}
+// while(userNum != gameNum) {
+//     userNum = prompt("You guessed the wrong number! Guess again:");
+// }
 
-alert("Congrats! you guessed it right!!")
+// alert("Congrats! you guessed it right!!")
+
+/* Q3. Prompt the user to enter their full name. Generate a username for them based on the user input. Start with @, followed by their full name and ending with their fullname length.
+Example: user input = "Akshit Mittal", so username should be "@akshit_mittal13"
+*/
+
+let str = prompt("Enter your full name:");
+let str1 = "@";
+let str2 = str.replaceAll(" ","_");
+let str3 = str.length;
+let username = str1+str2.toLowerCase()+str3;
+console.log(`Suggested username: ${username}`);
