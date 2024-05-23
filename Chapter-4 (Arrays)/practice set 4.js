7129b3cc09d28f4f07064a7216caef84122fd5a6
The-Knight-King <akshitmittal98@gmail.com>
2024-05-23 13:35:33 +0530

    Completed PQ-2 of practice set 4

--- Chapter-4 (Arrays)/practice set 4.js
+++ Chapter-4 (Arrays)/practice set 4.js
@@ -1,14 +1,27 @@
-// Q1. For a given array with marks of students -> [85,97,44,37,76,60]. Find the average marks of the entire class.
+// PQ1. For a given array with marks of students -> [85,97,44,37,76,60]. Find the average marks of the entire class.
 
-let marks = [85, 97, 44, 37, 76, 60];
-console.log("Marks of students:",marks);
-let sum = 0;
+// let marks = [85, 97, 44, 37, 76, 60];
+// console.log("Marks of students:",marks);
+// let sum = 0;
 
-for (let val of marks) {
-    sum += val;
+// for (let val of marks) {
+//     sum += val;
+// }
+
+// let average = (sum/marks.length);
+
+// console.log("Average marks of the class:",average);
+
+// PQ2. For a given array with prices of 5 items -> [250, 645, 300, 900, 50]. All items have an offer of 10% off on them. Change the array to store the final price of the items after applying the offer.
+
+// Method-1 for this PQ.
+
+let items = [250, 645, 300, 900, 50];
+
+for (let i=0; i<items.length; i++) {
+    console.log("Original price of item:",items[i]);
+    items[i] = items[i] * 0.9;
+    console.log("Discounted price after offer:",items[i],"\n\n");
 }
 
-let average = (sum/marks.length);
-
-console.log("Average marks of the class:",average);
-
+console.log("New price of the items:",items);
