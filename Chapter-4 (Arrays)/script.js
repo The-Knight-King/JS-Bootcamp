bfd179d9c9c287331f112e12ee67fae3b7b2758b
The-Knight-King <akshitmittal98@gmail.com>
2024-05-23 12:34:46 +0530

    Learning the basics of Array

---
+++ Chapter-4 (Arrays)/script.js
@@ -0,0 +1,19 @@
+// Arrays in JS
+
+let marks = [97, 67, 78, 34, 90, 89];
+console.log("Marks:",marks);
+console.log("length of the array is:",marks.length);
+console.log("Data type of Marks:",typeof(marks));
+
+// Array Indices
+// Indices in arrays starts at 0
+
+console.log("Accessing the array value at index 1:",marks[1]);
+
+// Values in arrays can be updated easily by simply accessing it from the index value and then assigning the new value.
+
+console.log("Marks array:",marks);
+marks[2] = 41;
+console.log("Replacing the value at index 2");
+console.log("Updated Marks:",marks);
+
