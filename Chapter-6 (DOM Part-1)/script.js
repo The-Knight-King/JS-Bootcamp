f960ae07368a0960e1d026af2fcb8a37da69d56d
The-Knight-King <akshitmittal98@gmail.com>
2024-05-24 20:39:23 +0530

    Learning DOM properties

--- Chapter-6 (DOM Part-1)/script.js
+++ Chapter-6 (DOM Part-1)/script.js
@@ -26,3 +26,27 @@ console.dir(firstClassEl);
 
 let allClassEl = document.querySelectorAll(".heading");
 console.dir(allClassEl);
+
+// DOM - Properties
+// tagName
+
+console.log("printing the first para:",firstEl);
+console.log("Printing the `tagName` of the first element:",firstEl.tagName);
+
+// innerText and innerHTML methods - they can be used to modify the text and HTML content of the page in the run time as well.
+
+let div = document.querySelector("div");
+console.dir(div);
+console.log("innerText method:",div.innerText);
+console.log("innerHTML method:",div.innerHTML);
+
+let heading = document.querySelector("h1");
+console.log("updating heading from JS:",heading);
+
+heading.innerText = "New heading";  //changing the heading text from here
+heading.innerHTML = "<i> New Heading </i>"  //Changing the HTML for this heading from here
+
+// textContent method - used to show text for the elements who have hidden visibility.
+
+let hidden = document.querySelector(".hidden");
+console.log("Showing the hidden heading:",hidden.textContent);
